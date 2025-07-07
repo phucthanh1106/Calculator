877e63a1f064fcc899bb5680df8876c3475a5ede
phucthanh1106 <tranpthanh2005@gmail.com>
2025-07-07 14:44:41 +0700

    Finished added more beautiful UI and completed C and negate button with some bugs fixed

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const negate = (a) => a * (-1);
 const equal = (a) => a;
 const point = (a) => a / 100;
 
@@ -28,9 +27,6 @@ const operate = (op, num1, num2 = 1) => {
         case "divide":
             res = divide(num1, num2);
             break;
-        case "negate":
-            res = negate(a);
-            break;
         case "equal":
             res = equal(num1);
             break;
@@ -46,15 +42,16 @@ const operate = (op, num1, num2 = 1) => {
 
 
 // Displaying
+let text1 = document.querySelector(".text1");
+let text2 = document.querySelector(".text2");
 const display = (content, line = 1) => {
-    let text1 = document.querySelector(".text1");
-    let text2 = document.querySelector(".text2");
     if (line == 1 || text1.textContent == "") {
         text1.textContent += content;
     } else {
         text2.textContent += content;
     }
 }
+
 
 // Creating buttons for the calculator
 const buttons = document.querySelectorAll("button");
@@ -64,10 +61,47 @@ buttons.forEach((button) => {
         // AC button
         if (button.dataset.clear == "clear") {
             reset()
+        // C button
+        } else if (button.dataset.clear == "backspace") {
+            text1.textContent = text1.textContent.trimEnd();
+            text1.textContent = text1.textContent.slice(0, text1.textContent.length - 1);
+        // Negate button
+        } else if (button.dataset.action) {
+            if (op == "") {
+                if (num1 == "") {
+                    num1 += "-";
+                    display("-", 1);
+                }
+                else if (+num1 > 0) {
+                    text1.textContent = "-" + text1.textContent;
+                    num1 = "-" + num1;
+                } else if (num1.includes("-")) {
+                    text1.textContent = text1.textContent.slice(1,);
+                    num1 = num1.slice(1,);
+                }
+            } else if (op == "sub") {
+                op = "add";
+                index = text1.textContent.indexOf("-");
+                text1.textContent = text1.textContent.slice(0, index) + "+" + text1.textContent.slice(index + 1);
+            } else if (num1 != "") {
+                if (num2 == "") {
+                    num2 += "-";
+                    display("-", 1);
+                }
+                else if (!num2.includes("-")) {
+                    opIndex = text1.textContent.lastIndexOf(" ");
+                    text1.textContent = text1.textContent.slice(0, opIndex + 1) + "-" + text1.textContent.slice(opIndex + 1, );
+                    num2 = "-" + num2;
+                } else if (num2.includes("-")) {
+                    lastIndex = text1.textContent.lastIndexOf("-");
+                    text1.textContent = text1.textContent.slice(0, lastIndex) + text1.textContent.slice(lastIndex + 1);
+                    num2 = num2.slice(1,);
+                }
+            }
         // num1 has not been fully written yet
         } else if (num1.length == 0  || (op.length == 0  && ((button.dataset.number || button.dataset.op == "point") && first))) {
             if (button.dataset.number) {
-                if (button.dataset.number != 0) {
+                if ((text1.textContent !== "0" && num1 !== "") || button.dataset.number != 0) {
                     num1 += button.dataset.number;
                     display(button.dataset.number, 1);
                 }  
@@ -80,18 +114,26 @@ buttons.forEach((button) => {
                 num1 += ".";
             }
         // Moving on the operator
-        } else if (button.dataset.op && op === "" && num2 === "" && button.dataset.op != "point" && button.dataset.op != "equal") {
+        } else if (button.dataset.op && num2 === "" && button.dataset.op != "point" && button.dataset.op != "equal") {
             if (!first) {
                 display(num1, 1);
             }
+
+            if (op == "") {
+                display(` ${button.textContent} `, 1);
+            } else {
+                num1Index = text1.textContent.indexOf(" ");
+                text1.textContent = text1.textContent.slice(0, num1Index) + ` ${button.textContent} `;
+            }
+
             op = button.dataset.op;
-            display(` ${button.textContent} `, 1);
-            console.log(num1,op,num2);
         // Finishing with the last number
         } else if (op != "" && (button.dataset.number || button.dataset.op == "point")) {
             if (button.dataset.number) {
-                num2 += button.dataset.number;
-                display(button.dataset.number, 1);
+                if ( num2 !== "" || button.dataset.number != 0) {
+                    num2 += button.dataset.number;
+                    display(button.dataset.number, 1);
+                }    
             } else if (button.dataset.op == "point" && !num2.includes(".") ) {
                 if (num2 == 0 || num2.length == 0) {
                     display("0.");
@@ -103,8 +145,6 @@ buttons.forEach((button) => {
         // Calculate the result and then reset every variable back to its default value
         } else if (button.dataset.op == "equal" && num2.length != 0) {
             let ans = operate(op, +num1, +num2);
-            let text1 = document.querySelector(".text1");
-            let text2 = document.querySelector(".text2");
 
             text1.textContent = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0";
             text2.textContent = ans;
@@ -120,14 +160,13 @@ buttons.forEach((button) => {
 // Create a reset function
 const reset = () => {
     first = true;
-    let text1 = document.querySelector(".text1");
-    let text2 = document.querySelector(".text2");
     text1.textContent = "";
     text2.textContent = 0;
     num1 = "";
     num2 = "";
     op = ""
 }
+
 
 
 
